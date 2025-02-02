6309209a4d15a5c236c287f47e5e65d7e79002e6
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-02-02 10:36:56 +0530

    static interface in java

---
+++ deafult method and static method/StaticInterface.java
@@ -0,0 +1,11 @@
+interface A{
+     static void show(){
+        System.out.println("hii");
+    }
+}
+
+public class StaticInterface {
+    public static void main(String[] args) {
+        A.show();
+    }
+}
